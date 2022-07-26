3d182cf1590a06c50a2da50f07852fb83418f2cf
Peter <peterwchusid@gmail.com>
2022-07-25 21:26:12 -0400

    07/25/22/2126

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//swiper js (testimonial section)
+const swiper = new Swiper(".swiper", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true,
+    },
+});
+
+
+
 const menuBtn = document.querySelector ('#menu.btn');
 const closeBtn = document.querySelector ('#close-btn');
 const menu = document.querySelector('nav .container ul');
@@ -19,9 +29,26 @@ closeBtn.addEventListener('click', () => {
 
 const navItems = menu.querySelectorAll('li');
 
+const changeActiveItem = () => {
+    navItems.forEach(item => {
+        const link = item.querySelector('a');
+        link.classList.remove('active');
+    })
+}
+
 navItems.forEach(item => {
     const link = item.querySelector('a');
     link.addEventListener('click', () => {
+        changeActiveItem();
         link.classList.add('active');
     })
+})
+
+
+//read more about
+const readMoreBtn = document.querySelector('read-more');
+const readMoreContent = document.querySelector('.read-more-content');
+
+readMoreBtn.addEventListener('click', () => {
+    readMoreContent.classList.toggle('show-content');
 })
