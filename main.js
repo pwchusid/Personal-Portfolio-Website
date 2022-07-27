2953d71bd68c200f20375ca2acb4d072dc5da52e
Peter <peterwchusid@gmail.com>
2022-07-26 20:31:05 -0400

    07/26/22/1931

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ const readMoreContent = document.querySelector('.read-more-content');
 readMoreBtn.addEventListener('click', () => {
     readMoreContent.classList.toggle('show-content');
     if(readMoreContent.classList.contains('show-content')){
-        readMoreBtn.textContent = "Show less"
+        readMoreBtn.textContent = "Show less";
     } else {
         readMoreBtn.textContent = "Show more";
     }
