1eccb5a2e7cf15a192970975988073735f6b941a
Peter <peterwchusid@gmail.com>
2022-08-30 22:36:52 -0400

    added intro animations

--- main.js
+++ main.js
@@ -62,5 +62,11 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
 })
 
-//canvas test
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".landing", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
